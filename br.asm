653b66f46731b905a3768bc3f7976730ce145fd7
Ruben Sørensen <ruben.sorensen98@gmail.com>
2023-12-21 02:58:43 +0100

    Using defined constants for increased readability

--- br.asm
+++ br.asm
@@ -1,7 +1,23 @@
+%define SYSREAD    0
+%define SYSWRITE   1
+%define SYSOPEN    2
+%define SYSCLOSE   3
+%define SYSACCESS 21
+%define SYSEXIT   60
+
+%define EXITSUCCESS 0
+%define EXITFAILURE 1
+
+%define STDIN  0
+%define STDOUT 1
+%define STDERR 2
+
 section .data
     buffer_size equ 4096
     no_argument_error db 'Error: No filename provided.', 0
+    no_argument_error_len equ $ - no_argument_error
     file_not_exists_error db 'File does not exist.', 0
+    file_not_exists_error_len equ $ - file_not_exists_error
     newline db 10
 
 section .bss
@@ -14,9 +30,9 @@ write_newline:
     ;; Inputs:
     ;; - rdi: File descriptor
     
-    mov rsi, newline                       ; Newline ASCII code
-    mov rdx, 1                             ; Length of the newline character
-    mov rax, 1                             ; Syscall number for write
+    mov rsi, newline            ; ASCII code for newline
+    mov rdx, 1                  ; Length of the newline character
+    mov rax, SYSWRITE
     syscall
     ret
     
@@ -27,9 +43,23 @@ write_message:
     ;; - rsi: Pointer to error message
     ;; - rdx: Length of error message
     
-    mov rax, 1                  ; Syscall number for write
+    mov rax, SYSWRITE
     syscall
     ret
+    
+exit_success:
+    mov rdi, EXITSUCCESS
+    jmp exit
+
+exit_failure:
+    mov rdi, EXITFAILURE
+    jmp exit
+
+exit:
+    ;; Inputs:
+    ;; - rdi: exit code
+    mov rax, SYSEXIT
+    syscall
     
 _start:
     ;; Check to see that there are at least two command line arguments
@@ -40,7 +70,7 @@ _start:
     ;; Check that file exists and is readable
     mov rdi, [rsp + 16]       ; Filename provided in argv[1]
     mov rsi, 0                ; F_OK
-    mov rax, 21               ; Syscall number for access
+    mov rax, SYSACCESS
     syscall
     cmp rax, 0                   ; Check if sys_open failed
     jne .file_not_exists_error   ; Jump to error handling if file doesn't exist
@@ -49,7 +79,7 @@ _start:
     mov rdi, [rsp + 16]         ; Filename provided in argv[1]
     mov rsi, 0                  ; O_RDONLY
     mov rdx, 0                  ; Mode is ignored when opening an existing file
-    mov rax, 2                  ; Syscall number for sys_open
+    mov rax, SYSOPEN            ; Syscall number for sys_open
     syscall
     mov r8, rax                 ; Save file descriptor
 
@@ -57,49 +87,37 @@ _start:
     mov rdi, r8                 ; File descriptor
     mov rsi, buffer
     mov rdx, buffer_size
-    mov rax, 0                  ; Syscall number for sys_read
+    mov rax, SYSREAD            ; Syscall number for sys_read
     syscall
 
     ;; Write to stdout
-    mov rdi, 1                  ; File descriptor for stdout
+    mov rdi, STDOUT             ; File descriptor for stdout
     mov rsi, buffer
     mov rdx, buffer_size
     call write_message
 
     ;; Close the file
     mov rdi, r8                 ; File descriptor
-    mov rax, 3                  ; Syscall number for sys_close
+    mov rax, SYSCLOSE           ; Syscall number for sys_close
     syscall
 
     call exit_success
     
 .no_argument_error:
     ;; Print the error to stderr
-    mov rdi, 2                  ; stderr
-    mov rsi, no_argument_error  ; Error message to write
-    mov rdx, 28                 ; Length of the error message
+    mov rdi, STDERR
+    mov rsi, no_argument_error
+    mov rdx, no_argument_error_len
     call write_message
     call write_newline
     jmp exit_failure
     
 .file_not_exists_error:
     ;; Print the error to stderr
-    mov rdi, 2                     ; stderr
-    mov rsi, file_not_exists_error ; Error message to write
-    mov rdx, 20                    ; Length of the error message
+    mov rdi, STDERR
+    mov rsi, file_not_exists_error
+    mov rdx, file_not_exists_error_len
     call write_message
     call write_newline
     jmp exit_failure
     
-    
-exit_success:
-    ; Exit the program
-    mov rax, 60 ; syscall number for sys_exit
-    xor rdi, rdi ; exit code 0
-    syscall
-
-exit_failure:
-    ; Exit the program with failure
-    mov rax, 60 ; syscall number for sys_exit
-    mov rdi, 1 ; exit code 1 (indicating failure)
-    syscall
